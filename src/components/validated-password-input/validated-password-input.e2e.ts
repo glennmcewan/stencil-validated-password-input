e61723afd0ea66ad894a61b6d3e9ff76e52bc85d
Glenn McEwan <glenn@web-dev.ninja>
2019-09-12 15:08:20 +0000

    Remove redundant tests

--- src/components/validated-password-input/validated-password-input.e2e.ts
+++ src/components/validated-password-input/validated-password-input.e2e.ts
@@ -8,25 +8,4 @@ describe('validated-password-input', () => {
     const element = await page.find('validated-password-input');
     expect(element).toHaveClass('hydrated');
   });
-
-  it('renders changes to the name data', async () => {
-    const page = await newE2EPage();
-
-    await page.setContent('<validated-password-input></validated-password-input>');
-    const component = await page.find('validated-password-input');
-    const element = await page.find('validated-password-input >>> div');
-    expect(element.textContent).toEqual(`Hello, World! I'm `);
-
-    component.setProperty('first', 'James');
-    await page.waitForChanges();
-    expect(element.textContent).toEqual(`Hello, World! I'm James`);
-
-    component.setProperty('last', 'Quincy');
-    await page.waitForChanges();
-    expect(element.textContent).toEqual(`Hello, World! I'm James Quincy`);
-
-    component.setProperty('middle', 'Earl');
-    await page.waitForChanges();
-    expect(element.textContent).toEqual(`Hello, World! I'm James Earl Quincy`);
-  });
 });
